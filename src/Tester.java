6ebd1fb60147a7343603faed375742579624a727
Tyrone <Tyrone>
2016-10-05 19:25:11 -0600

    Added a lot to getLastModified, currently it is connecting to a specified url and printing out the response of the server.  Later this functionality will be moved to getObject

--- src/Tester.java
+++ src/Tester.java
@@ -10,28 +10,43 @@ public class Tester {
 	
 	public static void main(String[] args) {
 
-		// include whatever URL you like
-		// these are just some samples
-		String[] url = {"people.ucalgary.ca/~mghaderi/index.html",
-						"people.ucalgary.ca/~mghaderi/test/uc.gif",
-						"people.ucalgary.ca/~mghaderi/test/a.pdf",
-						"people.ucalgary.ca:80/~mghaderi/test/test.html"};
-		
-		// this is a very basic tester
-		// the TAs will use a more comprehensive set of tests
-		try {
+		try{
 			UrlCache cache = new UrlCache();
 			
-			for (int i = 0; i < url.length; i++)
-				cache.getObject(url[i]);
+			cache.getLastModified("people.ucalgary.ca/~mghaderi/index.html");
+			cache.getLastModified("www.cbc.ca/radio/podcasts/index.html");
+			cache.Close();
+		}catch(UrlCacheException ex)
+		{
 			
-			System.out.println("Last-Modified for " + url[0] + " is: " + cache.getLastModified(url[0]));
-			cache.getObject(url[0]);
-			System.out.println("Last-Modified for " + url[0] + " is: " + cache.getLastModified(url[0]));
 		}
-		catch (UrlCacheException e) {
-			System.out.println("There was a problem: " + e.getMessage());
-		}
+		
+		
+//		// include whatever URL you like
+//		// these are just some samples
+//		String[] url = {"people.ucalgary.ca/~mghaderi/index.html",
+//						"people.ucalgary.ca/~mghaderi/test/uc.gif",
+//						"people.ucalgary.ca/~mghaderi/test/a.pdf",
+//						"people.ucalgary.ca:80/~mghaderi/test/test.html"};
+//		
+//		// this is a very basic tester
+//		// the TAs will use a more comprehensive set of tests
+//		try {
+//			UrlCache cache = new UrlCache();
+//			
+//			for (int i = 0; i < url.length; i++)
+//				cache.getObject(url[i]);
+//			
+//			System.out.println("Last-Modified for " + url[0] + " is: " + cache.getLastModified(url[0]));
+//			cache.getObject(url[0]);
+//			System.out.println("Last-Modified for " + url[0] + " is: " + cache.getLastModified(url[0]));
+//			
+//			cache.Close();
+//		}
+//		catch (UrlCacheException e) {
+//			System.out.println("There was a problem: " + e.getMessage());
+//		}
+		
 	}
 	
 }
--- src/UrlCache.java
+++ src/UrlCache.java
@@ -1,9 +1,19 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
+import java.util.Scanner;
 
 
 /**
@@ -15,6 +25,7 @@ import java.util.HashMap;
  */
 public class UrlCache {
 	private HashMap<String, Long> _Catalog;
+	private final int DEFAULT_HTTP_PORT = 80;
 		
     /**
      * Default constructor to initialize data structures used for caching/etc
@@ -23,7 +34,7 @@ public class UrlCache {
      * @throws UrlCacheException if encounters any errors/exceptions
      */
 	public UrlCache() throws UrlCacheException {
-		String catalogDir = System.getProperty("user.dir") + "\\Cache\\catalog";
+		String catalogDir = System.getProperty("user.dir") + "\\cache\\catalog";
 		Path path = Paths.get(catalogDir);
 		
 		System.out.println("Checking if catalog exists...");
@@ -31,9 +42,27 @@ public class UrlCache {
 		{
 			try{
 				FileInputStream fileIn = new FileInputStream(catalogDir);
+				ObjectInputStream objIn = new ObjectInputStream(fileIn);
+				
+				_Catalog = (HashMap<String, Long>)objIn.readObject();
+				
+				objIn.close();
+				fileIn.close();
 			}catch(FileNotFoundException ex)
 			{
-				//handle file not found, shouldn't get here - we checked that it existed
+				//file not found, shouldn't happen, we just checked if it existed.
+				System.out.println("Error reading catalog.  Error:" + ex.getMessage() + ". Creating empty catalog.");
+				_Catalog = new HashMap<String, Long>();
+			}catch(IOException ex)
+			{
+				//IOException opening input stream
+				System.out.println("Error reading catalog. Error:" + ex.getMessage() + ". Creating empty catalog.");
+				_Catalog = new HashMap<String, Long>();
+			}catch(ClassNotFoundException ex)
+			{
+				//ClassNotFoundException, shouldn't happen
+				System.out.println("Error reading catalog. Error:" + ex.getMessage() + ". Creating empty catalog.");
+				_Catalog = new HashMap<String, Long>();
 			}
 			
 			
@@ -55,7 +84,23 @@ public class UrlCache {
      * @throws UrlCacheException if encounters any errors/exceptions
      */
 	public void getObject(String url) throws UrlCacheException {
+		Scanner inputStream;
+		PrintWriter outputStream;
 		
+		try
+		{
+			Socket socket = new Socket(url, DEFAULT_HTTP_PORT);
+			outputStream = new PrintWriter(new DataOutputStream(socket.getOutputStream()));
+			inputStream = new Scanner(new InputStreamReader(socket.getInputStream()));
+			
+						
+			
+			inputStream.close();
+			outputStream.close();
+		}catch(Exception ex)
+		{
+			
+		}
 	}
 	
     /**
@@ -66,6 +111,39 @@ public class UrlCache {
      * @throws UrlCacheException if the specified url is not in the cache, or there are other errors/exceptions
      */
 	public long getLastModified(String url) throws UrlCacheException {
+		BufferedReader inputStream;
+		PrintWriter outputStream;
+		String response;
+		
+		try
+		{
+			//InetAddress addr = InetAddress.getByName("people.ucalgary.ca/~mghaderi/index.html");
+			//System.out.println(addr.getHostAddress());
+			String host = getHostnameFromUrl(url);
+			String objectPath = getObjectPathFromUrl(url);
+			String command = "GET " + objectPath + " HTTP/1.1\r\n";
+			
+			Socket socket = new Socket(host, DEFAULT_HTTP_PORT);  //explodes here, UnknownHostException
+			outputStream = new PrintWriter(socket.getOutputStream());
+			//ObjectInputStream objInStream = new ObjectInputStream(socket.getInputStream());
+			inputStream = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			
+			outputStream.print(command);
+			outputStream.print("Host: " + host + "\r\n");
+			outputStream.print("\r\n");
+			outputStream.flush();
+			
+			while((response = inputStream.readLine()) != null)
+				System.out.println(response);
+			
+			inputStream.close();
+			outputStream.close();
+			socket.close();
+		}catch(Exception ex)
+		{
+			System.out.println(ex.getMessage());
+		}
+		
 		return 0;
 	}
 	
@@ -74,6 +152,40 @@ public class UrlCache {
 	 */
 	public void Close()
 	{
+		String catalogDir = System.getProperty("user.dir") + "\\cache\\";
+		Path path = Paths.get(catalogDir);
 		
+		if(!Files.exists(path))
+		{
+			try{
+				Files.createDirectories(path);
+			}catch(IOException ex)
+			{
+				System.out.println("Error creating catalog directory.  Error:" + ex.getMessage() + ". Reopening this application will create new catalog.");
+			}
+		}
+		
+		try{
+			FileOutputStream fileOut = new FileOutputStream(catalogDir + "\\catalog");
+			ObjectOutputStream objOut = new ObjectOutputStream(fileOut);
+			
+			objOut.writeObject(_Catalog);
+			
+			objOut.close();
+			fileOut.close();
+		}catch(IOException ex)
+		{
+			System.out.println("Error writing catalog to file.  Error:" + ex.getMessage() + ". Reopening this application will create new catalog.");
+		}
+	}
+	
+	private String getHostnameFromUrl(String url)
+	{
+		return url.substring(0, url.indexOf("/") == -1 ? url.length() - 1 : url.indexOf("/"));
+	}
+	
+	private String getObjectPathFromUrl(String url)
+	{
+		return url.indexOf("/") == -1 ? "" : url.substring(url.indexOf("/"), url.length());
 	}
 }
